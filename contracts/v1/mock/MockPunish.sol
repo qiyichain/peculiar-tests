d6a67a17fe9c5e9640c02c53b8c74f7f23bdbdd2
yqq <youngqqcn@gmail.com>
2022-08-15 12:30:40 +0800

    add contracts code

---
+++ contracts/v1/mock/MockPunish.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+// #if Mainnet
+import "../Params.sol";
+// #else
+import "./MockParams.sol";
+// #endif
+import "../interfaces/IVotePool.sol";
+import "../interfaces/IValidators.sol";
+
+contract MockPunish is Params {
+    // clean validator's punish record if one restake in
+    function cleanPunishRecord(address)
+    external
+    view
+    onlyInitialized
+    returns (bool)
+    {
+        return true;
+    }
+}
